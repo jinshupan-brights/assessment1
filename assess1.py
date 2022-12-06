8e28e337eb9e8876dd4eab9dae6f6432033016c7
jisnhupan-brights <jinshu.pan@brights.com>
2022-12-06 10:55:18 +0100

    add function to fetch all data from db

--- assess1.py
+++ assess1.py
@@ -8,15 +8,34 @@ def connect_db():
         password="password",
         port = "5432" 
     )
-    print("connected")
     return assessment_db_connect
 
 
+def fetch_db_data(a):
+    cursor = connect_db().cursor()
+    cursor.execute(f"SELECT * FROM assessment1_7;")
+    db_data = cursor.fetchall()
+    cursor.close()
+    return db_data
+
+
+def disconnect():
+    connect_db().close()
+    print("Disconnected from the database. Good bye!")
+
+connect_db()
+print('''Welcome to the assessment database!\n
+Type [list] to list all data.
+Or type [quit] to exit the databse''')
+
 connected_to_db = True
 while connected_to_db:
-    connect_db()
-    print("Welcome to the assessment database! \n")
-    a = input("type 'quit' to exit the databse: ").lower().strip()
-    if a == "quit":
-        connect_db()
+    a = input("Please enter your command: ").lower().strip()
+    if a == "list":
+        db_data_list = fetch_db_data(connect_db)
+        for row in db_data_list:
+            print(row[0] + "\t" + row[1] + "\t" + row[2] + "\t" + row[3])
+    elif a == "quit":
+        disconnect()
         connected_to_db = False
+    
