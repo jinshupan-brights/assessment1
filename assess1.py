2c0f52e8d2300a8ab0865f251781e495b740f3b5
jisnhupan-brights <jinshu.pan@brights.com>
2022-12-06 10:31:27 +0100

    refined connection function

--- assess1.py
+++ assess1.py
@@ -1,17 +1,22 @@
 import psycopg2
 
-assessment_db_connect = psycopg2.connect(
-    host="localhost",
-    database="assessment1",
-    user="postgres",
-    password="password",
-    port = "5432" 
-)
+def connect_db():
+    assessment_db_connect = psycopg2.connect(
+        host="localhost",
+        database="assessment1",
+        user="postgres",
+        password="password",
+        port = "5432" 
+    )
+    print("connected")
+    return assessment_db_connect
+
 
 connected_to_db = True
 while connected_to_db:
+    connect_db()
     print("Welcome to the assessment database! \n")
     a = input("type 'quit' to exit the databse: ").lower().strip()
     if a == "quit":
-        assessment_db_connect.close()
+        connect_db()
         connected_to_db = False
